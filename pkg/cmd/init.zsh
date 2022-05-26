b3e153afe97a10d0befc3934719ee12cfcaa8f1d
Ryooooooga <eial5q265e5@gmail.com>
2022-05-26 19:12:58 +0900

    feat: fallback to default complete action

--- pkg/cmd/init.zsh
+++ pkg/cmd/init.zsh
@@ -1,5 +1,15 @@
-zle -N qwy::complete
+QWY_TRIGGER_KEY="${QWY_TRIGGER_KEY:-^I}"
+QWY_DEFAULT_COMPLETE="${QWY_DEFAULT_COMPLETE:-${$(\builtin bindkey "$QWY_TRIGGER_KEY")[(s: :w)2]}}"
+
+\builtin zle -N qwy::complete
 qwy::complete() {
-    eval "$(\command qwy expand --lbuffer="$LBUFFER" --rbuffer="$RBUFFER")"
-    zle reset-prompt
+    local out="$(\command qwy expand --lbuffer="${LBUFFER}" --rbuffer="${RBUFFER}")"
+    if [[ -n "${out}" ]]; then
+        \builtin eval "${out}"
+        \builtin zle reset-prompt
+    elif [[ -n "${QWY_DEFAULT_COMPLETE}" && "${QWY_DEFAULT_COMPLETE}" != "undefined-key" ]]; then
+        \builtin zle "${QWY_DEFAULT_COMPLETE}"
+    fi
 }
+
+\builtin bindkey "${QWY_TRIGGER_KEY}" qwy::complete
